22d14424365f78eee3e0f9401b73483526c5ac6c
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2020-05-21 08:36:32 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# like-every-video
-Like Every Video is a browser extension that likes every YouTube video watched by default to support content creators.
+# Like Every Video
+
+<div style="text-align:center"><img src="assets/social.png"/></div
+
+Like Every Video is a browser extension that defaults to liking every YouTube video watched
+to support content creators.
+This extension is made for people that are too lazy or too forgetful to like every video they watch. This does not prevent you from disliking or unliking, the default will just be a like.
---
+++ content.js
@@ -0,0 +1,56 @@
+const likeCallback = async() => {
+  // main controller, checks if youtube is open and if it is and the video is not liked 
+  // or dislike already it likes it.
+    
+  // wait for like and dislike buttons to show
+  const waitFor = (selector, interval) => {
+    return new Promise((res, rej) => {
+        waitForElementToDisplay(selector, interval);
+        function waitForElementToDisplay(selector, time) {
+            if (document.querySelectorAll(selector) != null) {
+                res(document.querySelectorAll(selector));
+            }
+            else {
+                setTimeout(function () {
+                    waitForElementToDisplay(selector, time);
+                }, time);
+            }
+        }
+    });
+  }
+
+  // get a list with like and dislike button. (like is first and dislike is second)
+  const [likeButton, dislikeButton] = await waitFor("#top-level-buttons > ytd-toggle-button-renderer", 600);
+
+  const is_rated = likeButton.classList.contains("style-default-active" ) || dislikeButton.classList.contains("style-default-active" );
+
+  // if the video is already rated then do nothing
+  if(!is_rated){
+    // click like
+    likeButton.click();
+  };
+}
+
+let enabled = true;
+browser.storage.sync.get("enabled", (state) => {
+  enabled = state.enabled;
+});
+
+browser.storage.onChanged.addListener( async(changes, areaname) => {
+  enabled = changes.enabled.newValue;
+  if(enabled)
+    await likeCallback();
+
+})
+
+const target = document.querySelector('title');
+
+const config = { attributes: true, childList: true, subtree: true };
+
+const observer = new MutationObserver(async (mutations, observer) => {
+  if(enabled)
+    await likeCallback();
+});
+
+// Start observing the title changes
+observer.observe(target, config);
